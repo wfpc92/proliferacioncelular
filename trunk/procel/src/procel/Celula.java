5e9b6d48f78716d525ffcd3f87ab3824e959f350
yohannazg31 <yohannazg31@4d353a02-2ef5-fca6-979f-dae1bfeac904>
2012-10-24 12:50:28 +0000

    Creacion de la clase celula con los atributos acordados

---
+++ trunk/procel/src/procel/Celula.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procel;
+
+public class Celula {
+    
+    int long_lado;
+    int num_lados;
+    
+
+    public int getLong_lado() {
+        return long_lado;
+    }
+
+    public void setLong_lado(int long_lado) {
+        this.long_lado = long_lado;
+    }
+
+    public int getNum_lados() {
+        return num_lados;
+    }
+
+    public void setNum_lados(int num_lados) {
+        this.num_lados = num_lados;
+    }
+    
+    
+}
