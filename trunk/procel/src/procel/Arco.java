15f022b7d844ad548b32df8dec1509bc322f4825
yohannazg31 <yohannazg31@4d353a02-2ef5-fca6-979f-dae1bfeac904>
2012-11-14 13:01:56 +0000

    Creacion de la clase celula con los atributos acordados

---
+++ trunk/procel/src/procel/Arco.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procel;
+
+/**
+ *
+ * @author yohanna
+ */
+public class Arco <T> {
+    private int vi;
+	private int vj;
+	private int costo;
+	
+	
+	Arco(int v1,int v2,int c){
+		vi=v1;
+		vj=v2;
+	    costo=c;
+	}
+	
+	public int getVi() {
+		return vi;
+	}
+	public void setVi(int vi) {
+		this.vi = vi;
+	}
+	public int getVj() {
+		return vj;
+	}
+	public void setVj(int vj) {
+		this.vj = vj;
+	}
+	public int getCosto() {
+		return costo;
+	}
+	public void setCosto(int costo) {
+		this.costo = costo;
+	}
+}
---
+++ trunk/procel/src/procel/Vertice.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procel;
+
+/**
+ *
+ * @author yohanna
+ */
+public class Vertice {
+    private int info;
+	private boolean marca;
+	private int subindice;
+	
+	Vertice(){
+		marca=false;
+	}
+	Vertice(int x){
+		info=x;
+		marca=false;
+	}
+	
+	public int getInfo() {
+		return info;
+	}
+	public void setInfo(int info) {
+		this.info = info;
+	}
+	public boolean getMarca() {
+		return marca;
+	}
+	public void setMarca(boolean marca) {
+		this.marca = marca;
+	}
+	public int getSubindice() {
+		return subindice;
+	}
+	public void setSubindice(int subindice) {
+		this.subindice = subindice;
+	}
+}
