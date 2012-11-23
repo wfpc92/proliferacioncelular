082f1c6ff89a43a5f96f85b19ba6ba9932933b62
jhreyes11 <jhreyes11@4d353a02-2ef5-fca6-979f-dae1bfeac904>
2012-11-23 16:18:39 +0000

    SE modifico: tejido y celula

--- trunk/procel/src/LogicaNegocio/Tejido.java
+++ trunk/procel/src/LogicaNegocio/Tejido.java
@@ -1,8 +1,9 @@
 package LogicaNegocio;
 
 public class Tejido<T> {
+    private int id;
     private Grafo<T> TejidoG;
-
+  
     public Grafo<T> getTejidoG() {
         return TejidoG;
     }
@@ -11,8 +12,27 @@ public class Tejido<T> {
         this.TejidoG = TejidoG;
     }
 
-    public Tejido(Grafo<T> TejidoG) {
+    public Tejido(){
+        this.TejidoG = null;
+        this.id = 0;
+    }
+    public Tejido(Grafo<T> TejidoG, int id) {
         this.TejidoG = TejidoG;
+        this.id = id;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
     }
   
 }
