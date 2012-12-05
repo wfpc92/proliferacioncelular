59c0a1b477d1fca53bb07aab8f44503dc3d468d9
wfpantoja <wfpantoja@4d353a02-2ef5-fca6-979f-dae1bfeac904>
2012-12-05 09:42:08 +0000

    se modifico de tal forma que permite visualizar la triangularizacion con las 500 celulas

--- procel/src/Vista/DibujadorCelula.java
+++ procel/src/Vista/DibujadorCelula.java
@@ -9,28 +9,47 @@ import java.awt.Polygon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import Modelo.Celula;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author ingesis
  */
-public class DibujadorCelula extends JPanel {
+public class DibujadorCelula extends JFrame {
     Celula celula = null;
     
     public DibujadorCelula (){
         super();
+        setVisible(true);
+        paint(getGraphics());
+        setExtendedState(MAXIMIZED_BOTH);
+        
     }
-    public void visualizar(Graphics g, Celula c){
-        super.paintComponent(g);
-        celula = c;
+    
+    
+    
+    @Override
+    public void paint(Graphics g){
+        celula = new Celula();
         Polygon poligono = new Polygon();
         int npoints = 0;
         npoints = asignarPuntos();
         
-        for (int i = 0; i < npoints; i++)
-            poligono.addPoint((int) (100 + 50 * Math.cos(i * 2 * Math.PI / npoints)),
-                                (int) (100 + 50 * Math.sin(i * 2 * Math.PI / npoints)));
+        for (int i = 0; i < npoints; i++){
+            try {
+                poligono.addPoint((int) (100 + 50 * Math.cos(i * 2 * Math.PI / npoints)),
+                                    (int) (100 + 50 * Math.sin(i * 2 * Math.PI / npoints)));
+                Thread.sleep(3000);
+            getGraphics().drawPolygon(poligono);
+            }
+            //g.fillPolygon(poligono); paa ponerle fondo
+            catch (InterruptedException ex) {
+                Logger.getLogger(DibujadorCelula.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
         
-        g.drawPolygon(poligono);
+        
         //g.fillPolygon(poligono); paa ponerle fondo
     }
     
